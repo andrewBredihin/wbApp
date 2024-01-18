70d62a7110c85fa55e4415db46ef79779b6b0142
Andrew <andrew.bredikhin.64@gmail.com>
2024-01-18 10:52:38 +0400

    Auth fix

--- app/src/main/java/com/bav/wbapp/appModuleDI.kt
+++ app/src/main/java/com/bav/wbapp/appModuleDI.kt
@@ -1,6 +1,7 @@
 package com.bav.wbapp
 
-import com.bav.wbapp.auth.AuthViewModel
+import com.bav.wbapp.auth.login.LoginViewModel
+import com.bav.wbapp.auth.registration.RegistrationViewModel
 import com.bav.wbapp.main.MainScreenViewModel
 import com.bav.wbapp.profile.ProfileViewModel
 import com.bav.wbapp.profile.edit.ProfileEditViewModel
@@ -10,7 +11,8 @@ import org.koin.dsl.module
 fun appModule() = module {
 
     viewModel { MainScreenViewModel() }
-    viewModel { AuthViewModel(get()) }
     viewModel { ProfileViewModel(get(), get()) }
     viewModel { ProfileEditViewModel(get()) }
+    viewModel { LoginViewModel(get()) }
+    viewModel { RegistrationViewModel(get()) }
 }
